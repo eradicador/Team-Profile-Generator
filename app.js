8354ff55879f3ef3b22664da7e60f990042471a6
eradicador <janisepinto@yahoo.com>
2020-07-08 22:31:34 -0400

    fixed the switch statements

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
 const teamMembers = [];
-
 const managerQuestions = [
     {
         type: "input",
@@ -27,7 +23,6 @@ const managerQuestions = [
         type: "input",
         name: "email",
         message: "enter your email",
-
     },
     {
         type: "input",
@@ -53,7 +48,6 @@ function newEmployee() {
                 "Intern",
                 "None"
             ]
-
         }
         // create a switch statement
     ]).then(function (answers) {
@@ -66,6 +60,7 @@ function newEmployee() {
                         name: "github"
                     }
                 ]);
+                break;
             case "Intern":
                 inquirer.prompt([
                     {
@@ -73,37 +68,37 @@ function newEmployee() {
                         message: "What school do you go to?",
                         name: "school"
                     }
-
                 ]);
-                break
-
+                break;
+            default:
+                console.log("Bad Input");
         };
-    }
+    })
+};
 
 
-//routes two more functions create Engineer and create employee 
 //if they select none you need a function to write to file with fs.
 createManager();
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
+// After the user has input all employees desired, call the render function (required
+// above) and pass in an array containing all employee objects; the render function will
 // generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
+// returned from the render function. Now write it to a file named team.html in the
+// output folder. You can use the variable outputPath above target this location.
+// Hint: you may need to check if the output folder exists and create it if it
 // does not.
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! 
+// for the provided render function to work!
+
+
+
+
+
